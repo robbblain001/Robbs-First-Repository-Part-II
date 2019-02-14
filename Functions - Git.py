b0516431d0e0245ee075c32bddc622ae9420447b
Robb Blain <02robb.blain@gulllakecs.org>
2019-02-14 09:33:46 -0500

    Add Functions & Variable

--- Functions - Git.py
+++ Functions - Git.py
@@ -1,7 +1,7 @@
 #4.13.3: Greetings
 #Robb Blain
 #2.6.19
-
+'''
 name = input("What is your name: ")
 
 def greeting():
@@ -9,3 +9,25 @@ def greeting():
 
 
 greeting()
+
+'''
+
+
+#4.13.4: Functions and Variables
+#Robb Blain
+#2.14.19
+
+
+
+
+x = 406
+
+def print_something():
+    x = 13
+    print(x)
+
+
+
+print_something()
+
+print(x)
