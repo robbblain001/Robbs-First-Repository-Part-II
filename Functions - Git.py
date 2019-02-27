7da6a787fd2c7952f2fe2e947c1600483e63e41b
Robb Blain <02robb.blain@gulllakecs.org>
2019-02-27 10:26:03 -0500

    COmmit for Github

--- Functions - Git.py
+++ Functions - Git.py
@@ -1,7 +1,7 @@
 #4.13.3: Greetings
 #Robb Blain
 #2.6.19
-'''
+
 name = input("What is your name: ")
 
 def greeting():
@@ -10,15 +10,10 @@ def greeting():
 
 greeting()
 
-'''
-
 
 #4.13.4: Functions and Variables
 #Robb Blain
 #2.14.19
-
-
-
 
 x = 11
 
@@ -31,3 +26,27 @@ def print_something():
 print_something()
 
 print(x)
+
+#4.13.5: Functions and Variables part 2
+#Robb Blain
+#2.14.19
+
+my_variable = 3.6745
+
+def something():
+    print(my_variable + 10)
+
+something()
+
+#4.13.6: Functions And Vareables part 3
+#Robb Blain
+#2.18.19
+
+
+def print_number(x):
+    print(str(x))
+
+
+print_number(12)
+print("")
+print_number('Hi')
